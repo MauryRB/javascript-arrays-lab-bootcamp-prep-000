ddc044a0ce385f64c96b8524eae1b5e00cd2b902
Amaury Burgos <amauryb.1204@gmail.com>
2020-11-11 19:31:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+
+function destructivelyprependKitten(name) {
+  return kittens.unshift(name)
+}
