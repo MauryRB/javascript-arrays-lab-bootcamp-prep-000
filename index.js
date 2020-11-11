51867c3286e03399a019e38b435755c92d61ce28
Amaury Burgos <amauryb.1204@gmail.com>
2020-11-11 19:35:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
-
-function destructivelyAppendKitten(name) {
-  return kittens.push(name)
-}
-
-function destructivelyPrependKitten(name) {
-  return kittens.unshift(name)
-}
-
-function destructivelyRemoveLastKitten() {
-  return kittens.pop(name)
-}
+var kittens = ["Milo", "Otis", "Garfield"] 
